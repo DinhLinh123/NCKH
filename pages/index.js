ffd572e279011904905d8c714b270bc738689aa6
linh <linhte242000@gmail.com>
2021-03-22 17:58:15 +0700

    add

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import Head from 'next/head';
+import styled from 'styled-components';
+
+const Div = styled.div`
+  
+  color: yellow;
+  width: 30px;
+  height: 30px;
+  background: red;
+`;
+
+// éo hiếu sao styled không vào
+
+export default function Home() {
+  return (
+
+      <Div>111</Div>
+ 
+  )
+}
+// chắc ko đc r
+
